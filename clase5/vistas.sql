00e7651c32ddde88309653d6975df0bdc2960105
Marcos <exegeses@gmail.com>
2024-03-01 11:41:04 -0300

    actualización notas vistas + Generación de backup

---
+++ clase5/vistas.sql
@@ -0,0 +1,29 @@
+# Vistas
+
+-- una vista es una consulta
+-- que podemos almacenar dentro de nuestra base de datos
+
+/* el objetivo es no tener que escribir varias veces
+   una misma consulta si tuviéramos que ejecutarla
+   varias veces en el día/semana/mes
+ */
+
+#! Parece una tabla independiente, pero en realidad no lo es
+
+CREATE VIEW consulta_precios
+    AS
+    SELECT idProducto AS id,
+           prdNombre AS Producto,
+           prdPrecio AS Contado,
+           format( prdPrecio * 1.05, 2 ) AS 'Precio Lista',
+           mkNombre AS Marca,
+           catNombre AS Categoría
+        FROM productos AS p
+             JOIN marcas AS m
+                  ON p.idMarca = m.idMarca
+             JOIN categorias AS c
+                  ON p.idCategoria = c.idCategoria;
+
+/* listar las vistas dentro de una base de datos */
+SHOW FULL TABLES IN introdb
+    WHERE TABLE_TYPE = 'VIEW';
